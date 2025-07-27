19c1ab74fbc7637c7782d7f46e9b92d880d29642
kvrapuru12 <kvrapuru12@gmail.com>
2025-07-27 13:40:52 +0100

    CRITICAL FIX: Enable Flyway migrations - tables were not being created causing 502 errors

--- src/main/java/com/healthapp/config/FlywayConfig.java
+++ src/main/java/com/healthapp/config/FlywayConfig.java
@@ -1,11 +1,8 @@
 package com.healthapp.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableAutoConfiguration(exclude = {FlywayAutoConfiguration.class})
 public class FlywayConfig {
-    // This configuration completely disables Flyway autoconfiguration
+    // Flyway will now run automatically and create the database tables
 } 
